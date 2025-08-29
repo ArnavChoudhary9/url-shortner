028393cbf269045db839928571e914b2fc002468
HotShot0901 <contact.hot.shor.1234@gmail.com>
2025-08-29 11:51:09 +0530

    Refactor Shorten component to remove hooks and streamline PocketBase authentication

--- src/app/shorten/page.tsx
+++ src/app/shorten/page.tsx
@@ -1,22 +1,13 @@
-'use client'
-
 import PocketBase from "pocketbase";
-import { useEffect, useMemo } from "react";
 
-export default function Shorten() {
-  const pb = useMemo(() => new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL), []);
+export default async function Shorten() {
+  const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_POCKETBASE_URL}/auth`, {
-        "credentials": "include"
-      })
-      const { record, token } = await res.json();
-      pb.authStore.save(token, record);
-    }
-
-    fetchData();
-  }, [pb]);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_POCKETBASE_URL}/auth`, {
+    "credentials": "include"
+  })
+  const { record, token } = await res.json();
+  pb.authStore.save(token, record);
 
   return (
     <div>
